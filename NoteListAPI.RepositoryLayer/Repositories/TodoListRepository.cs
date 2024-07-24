c92383d1cd2bd8de74bcb1f62bf11b9c4ee3d599
sajjat-hossen <sajjat.hossen@brainstation-23.com>
2024-07-24 13:08:56 +0600

    Add TodoListRepository

---
+++ NoteListAPI.RepositoryLayer/Repositories/TodoListRepository.cs
@@ -0,0 +1,21 @@
+﻿using NoteListAPI.DomainLayer.Data;
+using NoteListAPI.DomainLayer.Models;
+using NoteListAPI.RepositoryLayer.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteListAPI.RepositoryLayer.Repositories
+{
+    public class TodoListRepository : Repository<TodoList>, ITodoListRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public TodoListRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
--- NoteListAPI/Program.cs
+++ NoteListAPI/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddAutoMapper(typeof(Mappings));
 
 builder.Services.AddScoped<INoteRepository, NoteRepository>();
 builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddScoped<ITodoListRepository, TodoListRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
